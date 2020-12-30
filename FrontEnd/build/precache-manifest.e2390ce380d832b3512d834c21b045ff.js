2428ecf53e77e714e3c6624863b57b5c7d528a06
kirill <kirill.shakhov14@gmail.com>
2020-12-30 05:09:44 +0300

    fix

---
+++ FrontEnd/build/precache-manifest.e2390ce380d832b3512d834c21b045ff.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5aeb802c7dec7f4d56c585c4a7bc353d",
+    "url": "/~s285896/index.html"
+  },
+  {
+    "revision": "a5202097d0fee2e230e6",
+    "url": "/~s285896/static/css/main.df6e911d.chunk.css"
+  },
+  {
+    "revision": "dc4e54f9d8f36f02f08e",
+    "url": "/~s285896/static/js/2.bd94ad5c.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/~s285896/static/js/2.bd94ad5c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a5202097d0fee2e230e6",
+    "url": "/~s285896/static/js/main.3d0ba0cf.chunk.js"
+  },
+  {
+    "revision": "6d53d732bc2cbde31e4b",
+    "url": "/~s285896/static/js/runtime-main.a26d55f1.js"
+  }
+]);
