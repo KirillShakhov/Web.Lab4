857e621b4596d962e35a4fcd9e91e58d8d747214
kirill <kirill.shakhov14@gmail.com>
2021-01-15 02:34:47 +0300

    init lab(new)

--- FrontEnd/src/utils/config.ts
+++ FrontEnd/src/utils/config.ts
@@ -2,4 +2,4 @@
 export const ApiHost = 'http://localhost:11000/weblab4/api'; //http://localhost:11000/weblab4/api
 export const ApiVersion = 'v1';
 
-export const LocalAuthID = 'auth_user';
+export const LocalAuthID = 'mySecretKeyGoesHere';
